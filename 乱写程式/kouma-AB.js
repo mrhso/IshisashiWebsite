6354962189889625f33a7549cd64b3ae6cb20e4f
mrhso <liu.songquan@live.cn>
2020-02-10 19:48:24 +0800

    fix

--- 乱写程式/kouma-AB.js
+++ 乱写程式/kouma-AB.js
@@ -11,18 +11,18 @@ for (let file of files) {
     let header = wav.slice(0, 44);
     let length = header.readUInt32LE(40);
     let data = wav.slice(44, length + 44);
-    let meta = wav.slice(length, wav.length);
+    let meta = wav.slice(length + 44, wav.length);
 
     let pcm0 = data.slice(0, loop);
     let header0 = Buffer.from(header);
-    header0.writeUInt32LE(loop + 36, 4);
+    header0.writeUInt32LE(loop + meta.length + 36, 4);
     header0.writeUInt32LE(loop, 40);
     let wav0 = Buffer.concat([header0, pcm0, meta]);
     fs.writeFileSync(`${file}-0.wav`, wav0);
 
     let pcm1 = data.slice(loop, end);
     let header1 = Buffer.from(header);
-    header1.writeUInt32LE(end - loop + 36, 4);
+    header1.writeUInt32LE(end - loop + meta.length + 36, 4);
     header1.writeUInt32LE(end - loop, 40);
     let wav1 = Buffer.concat([header1, pcm1, meta]);
     fs.writeFileSync(`${file}-1.wav`, wav1);
