9eb90b13fe17083d9585a4ee900ff8ca61c6a62c
mrhso <liu.songquan@live.cn>
2020-05-20 18:01:17 +0800

    BW
    SoXR 的 BW 可以比 SoX 高很多，可能是因为 SoX 有别的处理。

--- 乱写程式/resample.js
+++ 乱写程式/resample.js
@@ -9,7 +9,7 @@ const { parseWAV, parseFmt, writeWAV, writeFmt } = require('./wavHandler.js');
 
 let soxr = '3-options-input-fn';
 let rate = 48000;
-let bandwidth = 99.98;
+let bandwidth = 99.999;
 
 let files = ['th06_01', 'th06_02', 'th06_03', 'th06_04', 'th06_05', 'th06_06', 'th06_07', 'th06_08', 'th06_09', 'th06_10', 'th06_11', 'th06_12', 'th06_13', 'th06_14', 'th06_15', 'th06_16', 'th06_17'];
 
