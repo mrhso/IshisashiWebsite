4f605a51e0a2bcfe34f6ecff1e1db4a2a1fbe741
mrhso <liu.songquan@live.cn>
2021-04-21 21:43:49 +0800

    th075pos

---
+++ 乱写程式/th075pos.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { parseWAV } = require('./wavHandler.js');
+
+let files = ['00a', '00b', '00c', '01a', '01b', '02a', '02b', '03a', '03b', '04a', '04b', '05a', '05b', '06a', '07a', '08a', '09a', '51', '52', '53', '54', '56', '57', '58', '59', '60', '61', '62', '63', '65', '67', '68', 'sys00_op', 'sys99_ed'];
+
+for (let file of files) {
+    console.log(`${file}.pos`);
+    let wav = parseWAV(fs.readFileSync(`${file}.wav`));
+
+    let loop = wav['cue '].readUInt32LE(24);
+    let end = loop + wav.LIST.readUInt32LE(16);
+
+    let pos = Buffer.alloc(8);
+    pos.writeUInt32LE(loop);
+    pos.writeUInt32LE(end, 4);
+    fs.writeFileSync(`${file}.pos`, pos);
+};
